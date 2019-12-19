6c37b1d07daa8e152506b04e3b87fb97b9dfbafd
Gauri <gauri.kangai@outlook.com>
2019-12-19 20:53:23 +0000

    Playing with React for first time

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Header extends React.Component {
+    render() {
+      return (
+          <div>
+        <h1>This is my title</h1>
+        <h2>This is my subtitle</h2>
+        </div>
+      );
+    }
+  }
+  export default Header;
