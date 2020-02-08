92bf496ecb9384f001050a8cb71c0d107b7593b8
Gauri <gauri.kangai@outlook.com>
2020-02-08 15:04:37 +0000

    Introducing onClick events

--- src/ActionButtons.js
+++ src/ActionButtons.js
@@ -4,12 +4,26 @@ import './App.css';
 
 
 class ActionButtons extends React.Component {
+
+
+editClicked = () => {
+    alert("Do you want to edit your task?");
+}
+
+deleteClicked = () => {
+    alert("Are you sure you want to delete this button?");
+}
+
+doneClicked = () => {
+    alert("Well done, you've completed this task!");
+}
+
     render() {
         return (
             
             <div className="col-9">
-                <button type="button">Update <i className="far fa-edit"></i></button> <button type="button">Delete <i
-                        className="far fa-trash-alt"></i></button> <button type="button">Done <i
+                <button type="button" onClick={this.editClicked}>Update <i className="far fa-edit"></i></button> <button type="button" onClick={this.deleteClicked}>Delete <i
+                        className="far fa-trash-alt"></i></button> <button type="button" onClick={this.doneClicked}>Done <i
                         className="far fa-paper-plane"></i></button>
             </div>
     
--- src/TaskCount.js
+++ src/TaskCount.js
@@ -8,13 +8,19 @@ class TaskCount extends React.Component {
 
     render() {
 
+        //the total function way?
         let TotalTaskCount = <TaskCount1 /> + 
         <TaskCount2 /> 
 
+        //or the props way?
+        //let TotalTaskCount = ({this.props.taskCount1} + {this.props.taskCount2});
+
         return (
 
+            //How to print it on the page? This way doesn't print the total number,
+            //but instead prints in the browser the above function details or [object Object]
             <div className="col-12">
-                <h1>TotalTaskCount</h1>
+                <h1>{TotalTaskCount}</h1>
 
             </div>
         );
