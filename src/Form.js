e635240ad6dbbabb6b6434bc9132f673a3b30b2d
Gauri <gauri.kangai@outlook.com>
2020-03-03 21:50:39 +0000

    Form component optimised

--- src/Form.js
+++ src/Form.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import TaskBox from './TaskBox';
 
 class Form extends React.Component {
 
+  state = {
+    taskDescription: "Enter your task..."
+  }
 
   addTask = () => {
-    this.props.addTaskFunc("Book train ticket");
+    this.props.addTaskFunc(this.state.taskDescription);
   }
 
 
   taskDescriptionChanged = (event) => {
     this.setState({
-   taskDescription: event.target.value
-    }); 
+      taskDescription: event.target.value
+    });
   }
 
 
@@ -23,66 +25,76 @@ class Form extends React.Component {
 
       <div className="box row col-lg-5">
 
-        <form id="taskForm">
-          <div className="form-group row">
 
+        <div className="form-group row taskForm">
+
+          <>
             <label for="newTask" className="col-lg-5 col-form-label-lg">New Task:</label>
 
-            <div className="col-lg-7">
+
+            <div className="col-lg-6">
               <input type="text" className="form-control" name="newTask" id="newTask"
-                placeholder="What's next to do?" required onChange={this.taskDescriptionChanged} ></input>
+                placeholder="What's next to do?" onChange={this.taskDescriptionChanged}></input>
             </div>
-          </div>
+          </>
 
-          <div className="form-group row">
 
+
+          <>
             <label for="categoryTask" className="col-lg-5 col-form-label-lg">Category:</label>
-            <div className="col-lg-7">
+            <div className="col-lg-6">
               <input type="text" class="form-control" name="categoryTask" id="categoryTask"
                 placeholder="Work? Personal?"></input>
             </div>
-          </div>
+          </>
 
 
-          <div className="form-group row">
 
+          <>
             <label for="priority" className="col-lg-5 col-form-label-lg">Priority:</label>
-            <div className="col-lg-3">
+            <div className="col-lg-2">
               <select className="form-control" name="priority" id="priority">
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
               </select>
             </div>
-          </div>
+            <div className="col-lg-1"></div>
 
-          <div className="form-group row">
+          </>
 
+
+          <>
             <label for="completeBy" className="col-lg-5 col-form-label-lg">Complete By:</label>
-            <div className="col-lg-7">
+            <div className="col-lg-6">
               <input type="date" className="form-control" name="completeBy" id="completeBy"></input>
 
             </div>
-          </div>
+          </>
 
-          <div className="form-group row">
 
-            <div className="col-lg-5">
+          <>
+            <div className="col-lg-4 my-3">
               <button type="submit" className="form-control" onClick={this.addTask}>Add</button>
             </div>
 
-          </div>
+          </>
 
-
-        </form>
+        </div>
       </div>
+
+
+
+
+
+
 
     );
 
-   
+
   }
 
- 
+
 }
 
 export default Form;
