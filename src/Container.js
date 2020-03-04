903a7b8d6813a0cee238f620f208738e62647203
Gauri <gauri.kangai@outlook.com>
2020-03-04 19:50:53 +0000

    Successfully updated client view when a task is completed

--- src/Container.js
+++ src/Container.js
@@ -128,6 +128,7 @@ class Container extends React.Component {
 
 
     render() {
+
         return (
 
             <div className="Container">
--- src/Task.js
+++ src/Task.js
@@ -20,18 +20,27 @@ class Task extends React.Component {
 
 
     render() {
+
+        let description;
+        
+        if (this.props.item.completed) {
+          description = <div className="completedTask">{this.props.item.description}</div>
+        } else {
+            description = <div>{this.props.item.description}</div>
+        }
+
+
         return (
 
             <>
                 <div className="col-4 taskEntry">
-                    {this.props.item.description}
+                {description}
                 </div>
-
                 <div className="col-2 categoryName">{this.props.item.category}</div>
                 <div className="col-3 goalDate">{this.props.item.goaldate}</div>
                 <div className="col-3">
                     <button type="button" onClick={this.editClicked}> <i className="far fa-edit"></i></button>  <button type="button" onClick={this.deleteClicked}> <i
-                        className="far fa-trash-alt"></i></button>  <button type="button" onClick={this.doneClicked}> <i
+                        className="far fa-trash-alt"></i></button> <button type="button" onClick={this.doneClicked}> <i
                             className="far fa-paper-plane"></i></button>
                 </div>
 
